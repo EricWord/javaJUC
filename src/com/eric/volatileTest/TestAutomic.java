21a8e2f146b264119cdb61bfc4eae6ff15c88184
tom_gl <xiaocui@163.com>
2019-03-06 16:38:53 +0800

    java多线程复习，原子性操作复习完毕

--- src/com/eric/volatileTest/TestAutomic.java
+++ src/com/eric/volatileTest/TestAutomic.java
@@ -1,5 +1,7 @@
 package com.eric.volatileTest;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @ClassName: TestAutomic
  * @Description: 测试原子性操作
@@ -8,4 +10,32 @@ package com.eric.volatileTest;
  * @Email: xiao_cui_vip@163.com
  */
 public class TestAutomic {
+    public static void main(String[] args) {
+        AutomaticDemo at = new AutomaticDemo();
+        for(int i=0;i<10;i++){
+            new Thread(at).start();
+        }
+
+    }
+
+
+}
+
+class AutomaticDemo implements Runnable {
+    private AtomicInteger sn = new AtomicInteger(0);
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(getSn());
+    }
+
+    public int getSn() {
+        return sn.getAndIncrement();
+    }
 }
